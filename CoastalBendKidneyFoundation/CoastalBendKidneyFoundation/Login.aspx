c2a4707e0e6e7dcfaf26c333d7e7713b7a3ccb51
hreeves19 <hreeves@islander.tamucc.edu>
2017-11-14 22:28:20 -0600

    Adding login (not finished yet)

--- CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Login.aspx
+++ CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Login.aspx
@@ -2,5 +2,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:sqldatasource runat="server" ConnectionString="<%$ ConnectionStrings:CostalbendKidneyFoundationConnectionString %>" SelectCommand="SELECT * FROM [User]"></asp:sqldatasource>
+    <asp:sqldatasource ID="userconnection" runat="server" ConnectionString="<%$ ConnectionStrings:CostalbendKidneyFoundationConnectionString %>" SelectCommand="SELECT * FROM [Admin]"></asp:sqldatasource>
+    <asp:SqlDataSource ID="adminconnection" runat="server" ConnectionString="<%$ ConnectionStrings:CostalbendKidneyFoundationConnectionString %>" SelectCommand="SELECT * FROM [Admin]"></asp:SqlDataSource>
+
+    <div class="main">
+        <div class="brand">
+            <h2>Login</h2>
+            <div class="textboxes">
+                <asp:TextBox ID="txtUsername" runat="server" placeHolder="Username" ></asp:TextBox>
+                <asp:TextBox ID="txtPassword" runat="server" placeHolder="Password" TextMode="Password"></asp:TextBox>
+            </div>
+
+            <div class="buttonSubmit">
+                <asp:Button ID="btnSubmit" runat="server" Text="Login" OnClick="btnSubmit_Click" />
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Login.aspx.cs
+++ CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CoastalBendKidneyFoundation
 {
@@ -12,6 +15,59 @@ namespace CoastalBendKidneyFoundation
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Local variables
+            bool pass = false;
+
+            // Creating database connection to user and admin
+            SqlConnection userdb = new SqlConnection(userconnection.ConnectionString);
+            SqlConnection admindb = new SqlConnection(adminconnection.ConnectionString);
+
+            // Creating commands
+            SqlCommand usercmd = new SqlCommand();
+            SqlCommand admincmd = new SqlCommand();
+
+            // Defining command type
+            usercmd.CommandType = System.Data.CommandType.Text;
+            admincmd.CommandType = System.Data.CommandType.Text;
+
+            admincmd.CommandText = "SELECT COUNT (Admin_ID) FROM Admin WHERE Admin_Username = '" + txtUsername.Text + "' AND" +
+                " Admin_Password = '" + txtPassword.Text + "'";
+
+            // Using try and catch block cause an exception can occur if something is wrong with database
+            try
+            {
+                admindb.Open();
+                Int32 result = (Int32)admincmd.ExecuteScalar();
+
+                // If there is a admin username and password that matches
+                if (result == 1)
+                {
+                    pass = true;
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('You have logged in!')", true);
+                }
+
+                else
+                {
+                    pass = false;
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Admin username or password is invalid!')", true);
+                }
+            }
+
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                // Print error message
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Error: A database error has occured.')", true);
+                pass = false;
+            }
+
+            finally
+            {
+                admindb.Close();
+            }
         }
     }
 }
