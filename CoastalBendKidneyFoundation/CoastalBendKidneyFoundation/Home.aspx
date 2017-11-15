c9ae8c54185f1705689f90fa2620f84348afffdf
hreeves19 <hreeves@islander.tamucc.edu>
2017-11-14 18:39:51 -0600

    Adding Home.aspx

---
+++ CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Home.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="CoastalBendKidneyFoundation.Home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:CostalbendKidneyFoundationConnectionString %>" SelectCommand="SELECT * FROM [Images]"></asp:SqlDataSource>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Timer1_Tick">
+            </asp:Timer>
+            <asp:Image ID="Image1" Height="200px" Width="200px" runat="server" />
+            <asp:Label ID="name" runat="server"></asp:Label>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Home.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CoastalBendKidneyFoundation
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        private int counter;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            counter = 1;
+
+            if(!IsPostBack)
+            {
+                SetImageUrl();
+            }
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            int i = (int) ViewState["ImageDisplayed"];
+            i = i + 1;
+            ViewState["ImageDisplayed"] = i;
+
+            DataRow imageDataRow = ((DataSet) ViewState["ImageData"]).Tables["image"].Select().FirstOrDefault(x => x["Img_Order"].ToString() == i.ToString());
+
+            if(imageDataRow != null)
+            {
+                Image1.ImageUrl = "~/images/" + imageDataRow["Img_Name"].ToString();
+                name.Text = imageDataRow["Img_Name"].ToString();
+            }
+
+            else
+            {
+                SetImageUrl();
+            }
+        }
+
+        private void SetImageUrl()
+        {
+            DataSet ds = new DataSet();
+
+            SqlConnection con = new SqlConnection(SqlDataSource1.ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter("spGetImageData", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.Fill(ds, "image");
+
+
+            ViewState["ImageData"] = ds;
+            ViewState["ImageDisplayed"] = 1;
+
+            DataRow imageDataRow = ds.Tables["image"].Select().FirstOrDefault(x => x["Img_Order"].ToString() == "1");
+            Image1.ImageUrl = "~/=images/" + imageDataRow["Img_Name"].ToString();
+
+        }
+    }
+}
