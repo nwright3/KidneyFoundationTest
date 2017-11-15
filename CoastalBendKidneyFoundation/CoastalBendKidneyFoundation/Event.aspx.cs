ff1987e0a68c1b53fde43e69cb0303815a527d4b
Omercan Devran <omercandevran@gmail.com>
2017-11-15 00:05:19 -0600

    I fully created event table insertation there are couple small things left

--- CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Event.aspx.cs
+++ CoastalBendKidneyFoundation/CoastalBendKidneyFoundation/Event.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,56 @@ namespace CoastalBendKidneyFoundation
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Get textboxes as strings
+            string ename = EvtName.Text;
+            string edate = EvtDate.Text;
+            string eplace = EvtPlace.Text;
+            string edescription = EvtDescription.Text;
+            
+
+            SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
+            SqlCommand cmd = new SqlCommand();                   //Creates database connection
+
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            cmd.CommandText = " INSERT INTO [Event] ([Event_Name] ,[Event_Date]  ,[Event_Place]  ,[Event_Description]) VALUES('" + ename + " ',' " + edate + " ',' " + eplace + " ',' " + edescription +  " ') ";
+          
+            cmd.Connection = db;
+            db.Open();
+
+
+            try
+            {       //Login created
+                cmd.ExecuteNonQuery();
+                EventUpdateLabel.Text = "Event created successfully!";
+                EventUpdateLabel.Visible = true;
+
+
+            }
+            catch
+            {           //error message if it doesnt work
+                EventUpdateLabel.Text = "An error occured while creating the event ";
+                EventUpdateLabel.Visible = true;
+
+
+            }
+            finally
+            {
+                db.Close();
+
+            }
+
+
+
 
         }
     }
