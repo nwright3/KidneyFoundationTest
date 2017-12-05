c7e6b9684aae5fcb684f2e675efa0cc8b23d33f7
Henry Reeves <hreeves@islander.tamucc.edu>
2017-12-04 21:31:15 -0600

    Here is more

---
+++ CoastalBendKidneyFoundation/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿/****** Object:  Table [dbo].[Comments]    Script Date: 12/4/2017 9:00:41 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Comments](
+	[Comment_Name] [nvarchar](50) NOT NULL,
+	[Comment] [nvarchar](1000) NOT NULL
+) ON [PRIMARY]
+GO
